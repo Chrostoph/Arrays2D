9e2226e46bec2fe5788f0098fc84037c09cfb725
Christopher_kiaz <svatzaika@gmail.com>
2022-10-18 14:51:37 +0200

    b variable added

---
+++ src/Arrays2D.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Arrays2D {
+    public int r, c, x, y;
+    int[][] arrayIn = new int [r][c];
+    int[] snail = new int[r*r];
+    public void readArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type in the number of rows of your matrice:");
+            r = sc.nextInt();
+        System.out.println("Type in the number of columns of your matrice:");
+            c = sc.nextInt();
+
+        arrayIn = new int [r][c];
+
+        System.out.println("Type in " + (r+c) + " numbers one after another:");
+            for (x = 0; x<r; x++){
+                for (y=0; y<c; y++){
+                    arrayIn[x][y] = sc.nextInt();
+                }
+            }
+    }
+
+    public void printArray (){
+        System.out.println("Your matrice is: ");
+        for (x = 0; x<r; x++){
+            for (y=0; y<c; y++){
+                System.out.print(arrayIn[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void snail () {
+        snail = new int[r*r];
+        for (int i = 0; i<r*r; i++){
+            System.out.println("Step");
+            for (int b = r; b>=0; b--){
+                for (y = 0; y<c; y++){
+                    snail [i] = arrayIn[x][y];
+                    i++;
+                }
+                y--;
+                for (x = c-y; x<b; x++) {
+                    snail [i] = arrayIn[x][y];
+                    i++;
+                }
+
+            }
+        }
+        for (int i : snail) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Arrays2D arrayIn = new Arrays2D();
+            arrayIn.readArray();
+            arrayIn.printArray();
+            arrayIn.snail();
+    }
+}
